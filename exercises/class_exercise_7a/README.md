95d9faf449faccd6a336757b12f8b217b0769d17
Divya Kulkarni <103066357+divya-kulkarni99@users.noreply.github.com>
2024-04-23 18:48:15 -0700

    Delete exercises/class_exercise_7a directory

--- exercises/class_exercise_7a/README.md
+++
@@ -1,3 +0,0 @@
-# class_exercise_7a
-
-Flutter + Firebase Storage: Upload, Retrieve, and Delete image files
--- exercises/class_exercise_7a/android/app/src/main/kotlin/com/example/class_exercise_7a/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.class_exercise_7a
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
